0596407fa9a6566629f1ce1e8503160d9aead5c0
Enzo Degraeve <enzodeg40@gmail.com>
2023-01-04 18:47:07 +0100

    Insertion d'item statique sur la page d'accueil

--- src/Controller/LeBonCubeController.php
+++ src/Controller/LeBonCubeController.php
@@ -11,8 +11,17 @@ class LeBonCubeController extends AbstractController
     #[Route('/', name: 'home')]
     public function index(): Response
     {
+        $item = [
+            'title' => 'Le Bon Cube',
+            'price' => '50.40',
+            'description' => 'Article',
+            'categorie' => 'Minerais',
+            'date' => '2023-01-04',
+        ];
+
         return $this->render('le_bon_cube/index.html.twig', [
             'controller_name' => 'LeBonCubeController',
+            'item' => $item,
         ]);
     }
 }
